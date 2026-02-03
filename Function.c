2f776c722d401528c7452859ce731f5237f9e0f2
mamun-forazi <aamalmamun99@gmail.com>
2026-02-03 23:00:01 +0600

    2 february

--- Function.c
+++ Function.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
 
+void calculatePower(double base, double exp)
+{
+    double  result=1, i;
+    for(i=1;i<exp; i++)
+    {
+        result= result * base;
+    }
+
+    printf("%lf\n", result);
+}
+
+
+int main(){
+    double base, exp;
+
+    printf("Enter Base = ");
+    scanf("%lf", &base);
+
+    printf("Enter Exponent = ");
+    scanf("%lf", &exp);
+
+
+    calculatePower(base, exp);
+
+
+return 0;
+
+}
